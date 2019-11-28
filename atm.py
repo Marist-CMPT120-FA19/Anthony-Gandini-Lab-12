eb2219133f8708ea0af3ad1a8eefcf7af9e437a8
Anthony Gandini <unconfigured@null.spigotmc.org>
2019-11-28 15:13:16 -0500

    update GUI

--- atm.py
+++ atm.py
@@ -56,20 +56,126 @@ class Account:
                 self.checking -= ammount
             else:
                 improperAmmount()
+                
+    def toString():
+        return string(self.ID + "\t" + self.PIN + "\t" + self.savings + "\t" + self.checking)
             
     def improperAmmount():
         print("The ammount requested is more than there is available.")
             
+def confirmationMenu(acc):
+    
+    def press(button):
+        if button == "Continue":
+            app.stop()
+            actionMenu(acc)
+        elif button == "Switch Account":
+            app.stop()
+            mainMenu()
+        else:
+            app.stop()
             
+    #updateFile(acc.toString() , acc)
+            
+    app = gui("Automated Teller Machine" , "400x200")
+    app.setBg("red")
+    
+    app.addLabel("confirm" , "Confirmation Page")
+    app.setLabelBg("confirm" , "white")
+    
+    app.addButtons(["Continue" , "Switch Account"] , press)
+    app.addButtons(["Quit"] , press)
+    
+    app.go()
+    
+def transactionMenu(type , boo , acc):
+
+    def press(button):
+        if boo:
+            if button == type + " savings":
+                app.stop()
+                confirmationMenu(acc)
+            elif button == type + " checking":
+                app.stop()
+                confirmationMenu(acc)
+        else:
+            if button == type + " savings to checking":
+                print(app.getEntry("Ammount"))
+                acc.transfer(True , app.getEntry("Ammount"))
+                app.stop()
+                confirmationMenu(acc)
+            elif button == type + " checking to savings":
+                acc.transfer(False , (app.getEntry("Ammount")))
+                app.stop()
+                confirmationMenu(acc)
+        if button == "Back":
+            app.stop()
+            actionMenu(acc)
+                
+    app = gui("Automated Teller Machine" , "400x200")
+    app.setBg("green")
+    
+    app.addLabelEntry("Ammount")
+    app.setFocus("Ammount")
+    
+    if boo:
+        app.addButtons([(type + " savings") , (type + " checking")] , press)
+        app.addButtons(["Back"] , press)
+    else:
+        app.addButtons([(type + " savings to checking") , (type + " checking to savings")] , press)
+        app.addButtons(["Back"] , press)
+    
+    app.go()
+
+def actionMenu(acc):
+
+    def press(button):
+        if button == "Deposit":
+            app.stop()
+            transactionMenu("To" , True , acc)
+        elif button == "Withdraw":
+            app.stop()
+            transactionMenu("From" , True , acc)
+        elif button == "Transfer":
+            app.stop()
+            transactionMenu("From" , False , acc)
+        else:
+            app.stop()
+            mainMenu()
+    
+    app = gui("Automated Teller Machine" , "400x200")
+    app.setBg("blue")
+    
+    app.addLabel("savingsBal" , ("Savings Account Balance: " + acc.getSavings()))
+    app.setLabelBg("savingsBal" , "white")
+    app.addLabel("checkingBal" , ("Checking Account Balance: " + acc.getChecking()))
+    app.setLabelBg("checkingBal" , "white")
+    app.addButtons(["Deposit" , "Withdraw" , "Transfer" , "Back"] , press)
+    
+    app.go()
+    
 def mainMenu():
     
     def press(button):
         if button == "Quit":
-            app.stop()
-    
+            app.stop()  
+        elif button == "Submit":
+            if checkCredentials(app.getEntry("User ID") , app.getEntry("PIN")):
+                accountsFile = open("accounts.txt" , "r")
+                for i in accountsFile:
+                    a = createAccount(i.split("\t"))
+                    if app.getEntry("User ID") == a.getID():
+                        if app.getEntry("PIN") == a.getPIN():
+                            accountsFile.close()
+                            app.stop()
+                            actionMenu(a)
+            else:
+                app.startSubWindow("error")
+                app.errorBox("loginError" , "Invalid login credentials!")
+                app.destroyAllSubWindows()
+            
     app = gui("Automated Teller Machine" , "400x200")
     app.setBg("orange")
-    app.setFont("90")
     
     app.addLabelEntry("User ID")
     app.addLabelSecretEntry("PIN")
@@ -79,31 +185,28 @@ def mainMenu():
     
     app.go()
     
-def main():
+def checkCredentials(id , pin):
+    accountsFile = open("accounts.txt" , "r")
+    for i in accountsFile:
+        a = createAccount(i.split("\t"))
+        if id == a.getID():
+            if pin == a.getPIN():
+                accountsFile.close()
+                return True
+        
+def createAccount(info):
+    return Account(info[0] , info[1] , info[2] , info[3])
     
+def updateFile(newLine , acc):
+    accountsFile = open("accounts.txt" , "r")
+    counter = 0
+    for i in accountsFile:
+        counter += 1
+        if i.split("\t")[0] == acc.getID:
+            break
+    
+def main():
     mainMenu()
     
 if __name__ == '__main__':
     main()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
